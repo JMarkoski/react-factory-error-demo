add00941601db2f6437167f7d4f898a9143920e6
Jovica Markoski <jovica.korcagin@gmail.com>
2020-09-30 23:57:17 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const TryFactory = React.createFactory("div"); // returns a react element, should work, RE type is 'createElementWithValidation'
-const TryBoundCreateElement = React.createElement.bind(null); // similar to above in design, RE type is also 'createElementWithValidation' still returns a react element and should work
 const TryFunctionComponent = () => React.createElement("div"); // obviously should work, RE type is 'TryFunctionComponent', like above two, returns a react element
 
 function App() {
